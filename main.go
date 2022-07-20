7e0b1f90de77c3c205c8858ecfca00a368f23be0
Klaus Großmann <klaus.grossmann@uplftd.com>
2022-07-20 11:08:12 +0200

    Update main.go

--- main.go
+++ main.go
@@ -22,8 +22,14 @@ package main
 
 import (
 	"go.k6.io/k6/cmd"
+    "github.com/DataDog/datadog-go/v5/statsd"
 )
 
 func main() {
+	statsd, err := statsd.New("127.0.0.1:8125", WithMaxBytesPerPayload(4096))
+    if err != nil {
+        log.Fatal(err)
+    }
+	
 	cmd.Execute()
 }
